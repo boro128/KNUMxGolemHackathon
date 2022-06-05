a27b0ac1d60b1df214b22d853ac647349bdf0999
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-06-05 14:23:33 +0200

    Extract custom dataset to separate class

--- magic_dataset.py
+++ magic_dataset.py
@@ -2,14 +2,13 @@ import os
 import torch
 import pandas as pd
 
-from typing import Optional, Callable
 from PIL import Image
+from typing import Optional, Callable
 
 
 class MagicDataset(torch.utils.data.Dataset):
-
     def __init__(
-        self,
+        self, 
         img_dir: str,
         labels_file: Optional[str] = None,
         transform: Optional[Callable] = None,
@@ -18,25 +17,26 @@ class MagicDataset(torch.utils.data.Dataset):
 
         self.img_dir = img_dir
         self.labels_file = labels_file
-        self.img_list = os.listdir(self.img_dir)
-        if labels_file is not None:
-            self.img_labels = pd.read_csv(labels_file).loc[:, "category_id"]
+        self.data = pd.read_csv(labels_file)
+        self.img_names = self.data.loc[:, "filename"]
+        self.img_labels = self.data.loc[:, "category_id"]
 
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self) -> int:
-        return len(self.img_list)
+        return len(self.img_names)
 
     def __getitem__(self, index: int):
-        img_path = os.path.join(self.img_dir, self.img_list[index])
-        image = Image.open(img_path)
+        img_path = os.path.join(self.img_dir, self.img_names[index])
+        image = Image.open(img_path).convert('RGB')
         label = self.img_labels[index]
 
         if self.transform:
             image = self.transform(image)
-
+            
         if self.target_transform:
             label = self.target_transform(label)
 
         return image, label
+    
